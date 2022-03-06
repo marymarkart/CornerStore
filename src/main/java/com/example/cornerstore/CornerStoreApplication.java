3f1d820b3f966cc23378e1e76b86cd12ba3abfb3
Mary <mary.markart@sjsu.edu>
2022-03-06 12:20:36 -0800

    Initial Commit

---
+++ src/main/java/com/example/cornerstore/CornerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.cornerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornerStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cornerstore/CornerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cornerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
